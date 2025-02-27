96df5e7f888bc5052a1b20ecc8377ad65333b57b
ShrinivasDev2 <shrinivastalawar@gmail.com>
2025-02-27 13:31:47 +0530

    home route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { getUsers, addUser } = require("../controllers/userController");
 const Router = express.Router();
 
+Router.get("/", (req, res) => {
+  res.send("Welcome to the server");
+});
 Router.get("/users", getUsers);
 Router.post("/add-user", addUser);
 
